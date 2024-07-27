64acebd67d5ab32f5561284fe028a09150347f6e
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-07-27 20:13:32 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    "test"
+  );
+}
+
+const Button = ({buttonValue}) => {
+  return <button className="btn btn-primary">{buttonValue}</button>
+}
+
+export default App;
